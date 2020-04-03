2245497016d0b4102b3c854953bec8498fd7918d
Graham Christensen <graham@grahamc.com>
2020-04-03 18:04:08 -0400

    rename to nixops_aws

---
+++ nixops_aws/plugin.py
@@ -0,0 +1,17 @@
+
+import os.path
+import nixops.plugins
+
+@nixops.plugins.hookimpl
+def nixexprs():
+    return [
+            os.path.dirname(__file__) + "/../nix"
+    ]
+
+@nixops.plugins.hookimpl
+def load():
+    return [
+        "nixops_aws.resources",
+        "nixops_aws.backends.ec2",
+        "nixops_aws.resources.ec2_keypair"
+    ]
